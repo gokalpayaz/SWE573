ca819b2277c21eba7f37d8eadcbdb1e55d09aa46
Gökalp Ayaz <gokalpayaz@windowslive.com>
2023-04-02 21:25:30 +0300

    fixed a typo in models.py

--- memories_app/models.py
+++ memories_app/models.py
@@ -15,7 +15,7 @@ class User(models.Model):
     password = models.CharField(max_length=128)
 
     def set_password(self, raw_password):
-        self.password_hash = make_password(raw_password)
+        self.password = make_password(raw_password)
     def check_password(self, raw_password):
         return check_password(raw_password, self.password)
 
