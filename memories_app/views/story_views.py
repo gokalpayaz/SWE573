633bea131394431bbbd934713fc449bd7d814ca9
Gökalp Ayaz <gokalpayaz@windowslive.com>
2023-05-09 01:43:07 +0300

    Handle save data in backend

--- memories_app/views/story_views.py
+++ memories_app/views/story_views.py
@@ -12,10 +12,12 @@ from django.urls import reverse_lazy
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from ..controllers.user_controller import UserController
-from ..models import Tags, Story, Location, StoryPhoto
+from ..models import Tags, Story, Location, StoryPhoto, Date
 from django.contrib.gis.geos import Point
 from django.core.files.uploadedfile import InMemoryUploadedFile
+from datetime import datetime
 
+date_format = '%Y-%m-%d'
 
 def create_post(request):
     # If user is submitting, request method will be post, if user is launching the page it will be get
@@ -27,6 +29,10 @@ def create_post(request):
         point = request.POST['location-point']
         radius = request.POST['radius']
         images = request.FILES.getlist('imageUpload[]')
+        date_option = request.POST['date_option']
+
+
+
         print(location_name)
         print(point)
 
@@ -39,6 +45,36 @@ def create_post(request):
         location.radius = float(radius)
         location.story = story
         location.save()
+
+        date = Date()
+        if date_option == "exact_date":
+            exact_date_str = request.POST['exact_date']
+            start_date = datetime.strptime(exact_date_str, '%Y-%m-%d').date()
+            end_date = start_date
+            date.start_date = start_date
+            date.end_date = end_date
+            date.year = start_date.year
+            date.season = get_season(start_date)
+        elif date_option == "interval":
+            start_date_str = request.POST['start_date']
+            end_date_str = request.POST['end_date']
+            start_date = datetime.strptime(start_date_str, '%Y-%m-%d').date()
+            end_date = datetime.strptime(end_date_str, '%Y-%m-%d').date()
+            date.start_date = start_date
+            date.end_date = end_date
+            avg_date = start_date + (end_date - start_date) / 2
+            date.year = avg_date.year
+            date.season = get_season(avg_date)
+        else:
+            date.year = request.POST['year']
+            date.season = request.POST['season']
+
+        date.story = story
+        date.save()
+
+
+
+
 
         for image in images:
             story_photo = StoryPhoto(story=story, photo=image)
@@ -54,3 +90,15 @@ def create_post(request):
     else:
 
         return render(request, 'memories/create_post.html')
+    
+
+
+def get_season(date):
+    if date.month == 12 or date.month < 3:
+        return 'W'
+    elif 3 <= date.month < 6:
+        return 'S'
+    elif 6 <= date.month < 9:
+        return 'U'
+    else:
+        return 'F'
